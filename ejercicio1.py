9da21669feb360ac63489f28b1cd43a949a71da8
koloman <facundo.kolomi@gmail.com>
2024-01-08 17:44:08 -0300

    ej1-terminado

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,6 +1,22 @@
 import random
 
+
 def createarray():
-    array = []
-    for _ in random.randint(0,10):
-        array.append(random.int(0, 1000))
+    numbers = []
+    for _ in range(random.randint(0,10)):
+        numbers.append(random.randint(0, 20))
+    return numbers
+
+
+def main():
+    result = 0
+    numbers = createarray()
+    for i in numbers:
+        if i < 13:
+            result += i
+    for i in numbers:
+        print(i)
+    print("La suma de los numeros fue", result)
+
+
+main()
