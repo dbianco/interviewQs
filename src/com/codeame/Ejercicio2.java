1bf27ccf10899a636ed563a5f8daed184c698555
Daniel Oliva Bianco <olivamau@amazon.com>
2024-01-12 11:17:04 -0300

    Ejercicio 2!

---
+++ src/com/codeame/Ejercicio2.java
@@ -0,0 +1,35 @@
+package com.codeame;
+
+public class Ejercicio2 {
+
+    /*Given three ints, a b c, one of them is small, one is medium and one is large.
+    Return true if the three values are evenly spaced, so the difference between
+    small and medium is the same as the difference between medium and large.
+    Sample: evenlySpaced(2, 4, 6) → true
+            evenlySpaced(4, 6, 2) → true
+            evenlySpaced(4, 6, 3) → false
+     */
+    public boolean evenlySpaced(int a, int b, int c) {
+
+    }
+
+    /* Given a string and a non-empty word string, return a string made of each char just before
+    and just after every appearance of the word in the string. Ignore cases where there is no char
+    before or after the word, and a char may be included twice if it is between two words.
+    Sample: wordEnds("abcXY123XYijk", "XY") → "c13i"
+    */
+    public String wordEnds(String str, String word) {
+        return null;
+    }
+
+    /* Given an array of ints, is it possible to choose a group of some of the ints,
+    such that the group sums to the given target?
+    Sample: groupSum([2, 4, 8], 10) → true
+            groupSum([2, 4, 8], 14) → true
+            groupSum([2, 4, 8], 9) → false
+     */
+    public boolean groupSum(int[] nums, int target) {
+        return false;
+    }
+
+}
